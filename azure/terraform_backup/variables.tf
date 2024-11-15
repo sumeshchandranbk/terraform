18b56f2ec2b3b286de31f2512e37a7e39bb4de11
sumeshchandranbk <44705560+sumeshchandranbk@users.noreply.github.com>
2024-11-15 16:11:57 +0200

    Add files via upload

---
+++ azure/terraform_backup/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group Name"
+
+}
+variable "resource_group_location" {
+  type        = string
+  description = "Resource group location"
+
+}
+variable "storage_account_name" {
+  type = string
+
+}
+variable "storage_container_name" {
+
+  type = string
+}
+
